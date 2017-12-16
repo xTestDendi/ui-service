d3c24e47c7d64275325cc0eac92f5752dd5f3afe
TsvetkovArtem <artem.tsvetkov.95@bk.ru>
2017-12-16 15:08:33 +0300

    I was create SpringBoot application with vaadin8 and Jetty server.

--- src/main/java/com/netcracker/ui/service/UiServiceApplication.java
+++ src/main/java/com/netcracker/ui/service/UiServiceApplication.java
@@ -1,12 +1,22 @@
 package com.netcracker.ui.service;
 
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.spring.annotation.SpringUI;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.UI;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UiServiceApplication {
-
-	public static void main(String[] args) {
+@SpringUI
+public class UiServiceApplication extends UI
+{
+	public static void main(String[] args) 
+        {
 		SpringApplication.run(UiServiceApplication.class, args);
 	}
+        @Override
+        protected void init(VaadinRequest vaadinRequest) {
+            setContent(new Label("Hello! I'm the root UI!"));
+        }
 }
